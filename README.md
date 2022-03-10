997071f03be3e54d8c0641df8efb3179b97277f0
jaebak <jbkim@ucsb.edu>
2022-03-10 14:57:39 -0800

    Added runCommand

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+Enviornment setup
+To setup python in unbuffer mode to print things quicker, use the below command.
+`export PYTHONUNBUFFERED=1`
+Or in the python scripts to run,  use the below shebang
+```
+#!/bin/sh
+''''exec python3 -u -- "$0" ${1+"$@"} # '''
+```
+
 Python script that runs commands
 - `run_command.py`
 
--- run_command_python2.py
+++ run_command_python2.py
@@ -3,37 +3,64 @@ from __future__ import print_function
 import subprocess
 import threading
 
-def output_reader(process, log_file):
+def output_reader_logger(process, log_file, commandOutput):
   for line in iter(process.stdout.readline, b''):
     output = line.decode('utf-8')
     print(output, end='')
     log_file.write(output)
+    commandOutput[0] += output
 
 # https://eli.thegreenplace.net/2017/interacting-with-a-long-running-child-process-in-python/
-def runAndLogCommand(command, log_file):
-  print("\nRunning command: "+command)
-  log_file.write("\nRunning command: "+command+'\n')
+def runCommandAndLog(command, log_file):
+  print("\n[Info] Running command: "+command)
+  log_file.write("\n[Info] Running command: "+command+'\n')
   process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
-  thread = threading.Thread(target=output_reader, args=(process,log_file))
+  commandOutput = ['']
+  thread = threading.Thread(target=output_reader_logger, args=(process,log_file, commandOutput))
   thread.start()
   thread.join()
-  return process.poll()
+  return process.poll(), commandOutput[0]
+
+
+def output_reader(process, commandOutput):
+  for line in iter(process.stdout.readline, b''):
+    output = line.decode('utf-8')
+    print(output, end='')
+    commandOutput[0] += output
+
+def runCommand(command):
+  print("\n[Info] Running command: "+command)
+  process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+  commandOutput = ['']
+  thread = threading.Thread(target=output_reader, args=(process,commandOutput))
+  thread.start()
+  thread.join()
+  return process.poll(), commandOutput[0]
 
 if __name__ == "__main__":
-  # Use 'python3 -u', which is unbuffer mode, to print things quicker
-  # To do so, use below shebang
-  # #!/bin/sh
-  # ''''exec python3 -u -- "$0" ${1+"$@"} # '''
-  # OR set environment with below settings
-  # export PYTHONUNBUFFERED=1
-
-  print("Script will execute: "+'python3 -u test_command.py')
+  print("[Info] Script will execute: "+'./test_command.py')
   with open('test_command.log', 'w') as log_file :
-    runAndLogCommand('./test_command.py', log_file)
-  print("Output and error logged to test_command.log\n")
+    returnCode, output = runCommandAndLog('./test_command.py', log_file)
+  print("[Info] Output and error logged to test_command.log\n")
+  print("[Info] Printing returnCode: "+str(returnCode))
+  print("[Info] Printing output to script")
+  print(output)
+  print()
 
-  print("Script will execute: "+'python3 -u crash_command.py')
+  print("Script will execute: "+'./crash_command.py')
   with open('crash_command.log', 'w') as log_file :
-    runAndLogCommand('./crash_command.py', log_file)
+    returnCode, output = runCommandAndLog('./crash_command.py', log_file)
   print("Output and error logged to crash_command.log")
+  print("[Info] Printing returnCode: "+str(returnCode))
+  print("[Info] Printing output to script")
+  print(output)
+
+  print("[Info] Script will execute: "+'./test_command.py')
+  returnCode, output = runCommand('./test_command.py')
+  print("[Info] Output and error logged to test_command.log\n")
+  print("[Info] Printing returnCode: "+str(returnCode))
+  print("[Info] Printing output to script")
+  print(output)
+  print()
